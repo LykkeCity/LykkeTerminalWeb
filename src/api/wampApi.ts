7aa7a117244b03c57ee49d3092e78b0afad965a5
g.moiseychik <g.moiseychik@itransition.com>
2017-12-21 13:31:28 +0300

    feature/implement-wamp-data-service: improved wamp service, fixed autobahn cbor library conflicts

--- src/api/wampApi.ts
+++ src/api/wampApi.ts
@@ -1,9 +1,53 @@
-const autobahn = require('autobahn');
+import autobahn = require('autobahn');
 
 export class WampApi {
   session: any;
 
-  connect = (url: string, realm: string) => {
+  constructor(url: string, realm: string) {
+    this.connect(url, realm);
+  }
+
+  subscribe = (topic: string, cb: any) => {
+    if (!this.session) {
+      setTimeout(() => {
+        this.subscribe(topic, cb);
+      }, 100);
+    } else {
+      this.session.subscribe(topic, cb);
+    }
+  };
+
+  publish = (topic: string, event: [any]) => {
+    if (!this.session) {
+      setTimeout(() => {
+        this.publish(topic, event);
+      }, 100);
+    } else {
+      this.session.publish(topic, event);
+    }
+  };
+
+  register = (topic: string, procedure: any) => {
+    if (!this.session) {
+      setTimeout(() => {
+        this.register(topic, procedure);
+      }, 100);
+    } else {
+      this.session.register(topic, procedure);
+    }
+  };
+
+  call = (topic: string, procedure: any) => {
+    if (!this.session) {
+      setTimeout(() => {
+        this.call(topic, procedure);
+      }, 100);
+    } else {
+      this.session.call(topic, procedure);
+    }
+  };
+
+  private connect = (url: string, realm: string) => {
     const connection = new autobahn.Connection({url, realm});
 
     connection.onopen = (session: any) => {
@@ -12,26 +56,6 @@ export class WampApi {
 
     connection.open();
   };
-
-  subscribe = (topic: string, cb: any) => {
-    this.session.subscribe(topic, cb);
-  };
-
-  publish = (topic: string, event: [any]) => {
-    this.session.publish(topic, event);
-  };
-
-  register = (topic: string, procedure: any) => {
-    this.session.register(topic, procedure);
-  };
-
-  call = (topic: string, procedure: any) => {
-    this.session.call(topic, procedure);
-  };
 }
-
-const wamp = new WampApi();
-wamp.connect('wss://wamp.lykke.com/ws/', 'prices');
-// wamp.subscribe('candle.spot.eurusd.mid.sec', function onEvent(args: [any]) {});
 
 export default WampApi;
