cc1578cc7e7da79da8bfe90a63aa81f36e84d1a8
George <igeorg3i@gmail.com>
2017-12-26 14:53:33 +0300

    Feature/implement wamp data service (#5)

--- src/components/BalanceList/index.tsx
+++ src/components/BalanceList/index.tsx
@@ -11,6 +11,7 @@ export interface BalanceListItemProps {
   profitAndLoss: number;
   symbol: string;
 }
+
 const ConnectedBalanceList = connect(
   ({balanceListStore: {allBalanceLists: balances}}) => ({
     balances
