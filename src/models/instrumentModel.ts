d2e86288be742ae5c74b16af8397144d9b03d12a
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-12-28 16:12:20 +0500

    Feature/select active instrument (#10)

--- src/models/instrumentModel.ts
+++ src/models/instrumentModel.ts
@@ -1,4 +1,5 @@
-import {extendObservable} from 'mobx';
+import {action, extendObservable, observable} from 'mobx';
+import Dir from '../stores/dir';
 import {AssetModel} from './index';
 
 class InstrumentModel {
@@ -9,9 +10,23 @@ class InstrumentModel {
   accuracy: number;
   invertedAccuracy: number;
 
+  @observable price: number;
+  @observable change: number;
+
+  @observable dir: Dir;
+
   constructor(instrument: Partial<InstrumentModel>) {
     extendObservable(this, instrument);
   }
+
+  @action
+  updatePrice = (nextPrice: number) => {
+    if (this.price !== nextPrice) {
+      this.change = (nextPrice - this.price) / this.price * 100;
+      this.dir = this.change > 0 ? Dir.Up : Dir.Down;
+      this.price = nextPrice;
+    }
+  };
 }
 
 export default InstrumentModel;
