03cada340c07c4ca3f768a5d2488d59e739b4c2c
sergeyschegolev <31923498+sergeyschegolev@users.noreply.github.com>
2017-12-29 03:49:08 +0300

    Feature/implement tabs (#8)

---
+++ src/components/Tile/TileContent.tsx
@@ -0,0 +1,90 @@
+import {lighten, rem} from 'polished';
+import * as React from 'react';
+import Icon from '../Icon/Icon';
+import styled, {css} from '../styled';
+import {TileTabItem} from './';
+
+// tslint:disable-next-line:no-var-requires
+const {Flex, Box} = require('grid-styled');
+
+interface TileContentProps {
+  tabs: any;
+  children: any;
+}
+
+interface TileContentState {
+  activeTabIndex: number;
+}
+
+const iconCss = css`
+  i {
+    cursor: pointer;
+    color: #d8d8d8;
+    &:hover {
+      color: ${lighten(0.2)('#d8d8d8')};
+    }
+  }
+`;
+
+const TileToolbar = styled.div`
+  border-bottom: solid 1px rgba(0, 0, 0, 0.2);
+  padding: ${rem(10)} 0;
+  ${iconCss};
+`;
+
+const StyledTileContent = styled.div`
+  font-size: ${rem(14)};
+  padding: ${rem(10)} ${rem(15)};
+`;
+
+class TileContent extends React.Component<TileContentProps, TileContentState> {
+  constructor(props: TileContentProps) {
+    super(props);
+    this.state = {
+      activeTabIndex: 0
+    };
+  }
+
+  clickHandler = (index: number) => {
+    if (!this.props.children.length) {
+      return;
+    }
+    this.setState({
+      activeTabIndex: index
+    });
+  };
+
+  render() {
+    const child = this.props.children.length
+      ? this.props.children[this.state.activeTabIndex]
+      : this.props.children;
+    return (
+      <StyledTileContent>
+        <TileToolbar>
+          <Flex align="center" justify="space-between">
+            <Flex align="center">
+              {this.props.tabs.map((tab: string, index: number) => {
+                return (
+                  <TileTabItem
+                    isClickable={!!this.props.children.length}
+                    key={`tiletabitem_${index}`}
+                    tabName={tab}
+                    index={index}
+                    activeIndex={this.state.activeTabIndex}
+                    click={this.clickHandler.bind(this, index)}
+                  />
+                );
+              })}
+            </Flex>
+            <Box>
+              <Icon name="cog" />
+            </Box>
+          </Flex>
+        </TileToolbar>
+        {child}
+      </StyledTileContent>
+    );
+  }
+}
+
+export default TileContent;
