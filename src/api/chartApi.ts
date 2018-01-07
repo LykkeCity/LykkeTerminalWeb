cc2d5e9034982ff4c4ca829858b07ef22320977f
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-01-08 01:26:00 +0500

    Initialize bars with historic data

--- src/api/chartApi.ts
+++ src/api/chartApi.ts
@@ -1,4 +1,5 @@
 import {Session} from 'autobahn';
+import wretch from 'wretch';
 
 // tslint:disable:no-console
 // tslint:disable:object-literal-sort-keys
@@ -6,7 +7,7 @@ import {Session} from 'autobahn';
 const symbol = (name = '') => ({
   name: 'BTCUSD',
   minmov: 1,
-  pricescale: 10,
+  pricescale: 1,
   minmove2: 2,
   session: '24x7',
   timezone: 'Europe/Istanbul',
@@ -24,7 +25,7 @@ const symbol = (name = '') => ({
     '1M'
   ],
   has_intraday: true,
-  intraday_multipliers: [1, 5, 15, 30, 60, 240, 360, 720],
+  intraday_multipliers: ['1', '5', '15', '30', '60', '240', '360', '720'],
   has_empty_bars: true
 });
 
@@ -61,11 +62,28 @@ class ChartApi {
     onErrorCallback: any,
     firstDataRequest: any
   ) => {
-    const bars: any[] = [];
     if (firstDataRequest) {
-      onHistoryCallback(bars, {
-        noData: bars.length === 0
-      });
+      wretch('https://public-api.lykke.com/api/Candles/history/btcusd/spot')
+        .json({
+          period: 'minute',
+          type: 'Bid',
+          // dateFrom: new Date(2018, 1, 1).toISOString(),
+          // dateTo: new Date(2018, 1, 7).toISOString()
+          dateFrom: new Date(new Date(Date.now()).setHours(-12)).toISOString(),
+          dateTo: new Date(Date.now()).toISOString()
+        })
+        .post()
+        .json()
+        .then(resp => {
+          const bars = resp.data
+            .map(mapAsBar)
+            .sort((a: any, b: any) => b.time - a.time)
+            .reverse();
+          onHistoryCallback(bars, {
+            noData: bars.length === 0,
+            nextTime: Math.max(bars.map((x: any) => x.time))
+          });
+        });
     }
   };
 
@@ -76,18 +94,9 @@ class ChartApi {
     subscriberUID: any,
     onResetCacheNeededCallback: any
   ) => {
-    this.session.subscribe('candle.spot.btcusd.bid.minute', (args: any[]) => {
+    this.session.subscribe('candle.spot.btcusd.bid.5min', (args: any[]) => {
       if (args) {
-        const {t, c, o, h, l, v} = args[0];
-        console.log(t, c, o, h, l, v);
-        onRealtimeCallback({
-          time: new Date(t).getTime(),
-          close: c,
-          open: o,
-          high: h,
-          low: l,
-          volume: v
-        });
+        onRealtimeCallback(mapAsBar(args[0]));
       }
     });
   };
@@ -116,5 +125,14 @@ class ChartApi {
     cb(Date.now());
   };
 }
+
+const mapAsBar = ({t, c, o, h, l, v}: any) => ({
+  time: new Date(t).getTime(),
+  close: c,
+  open: o,
+  high: h,
+  low: l,
+  volume: v
+});
 
 export default ChartApi;
