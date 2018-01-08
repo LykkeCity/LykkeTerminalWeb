7c0942620ce6a7625b68840d1c84e825dc725e62
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-01-08 15:17:44 +0500

    Chart tyle improvements

--- src/chart.ts
+++ src/chart.ts
@@ -2,6 +2,11 @@ import {ChartApi} from './api/index';
 
 // tslint:disable:object-literal-sort-keys
 
+const LINESTYLE_DOTTED = 1;
+const LINESTYLE_DASHED = 2;
+// const LINESTYLE_SOLID = 0;
+// const LINESTYLE_LARGE_DASHED = 3;
+
 const config = {
   exchanges: [],
   supported_resolutions: [
@@ -20,30 +25,37 @@ const config = {
 };
 
 export default (session: any) => {
-  const w = window as any;
-  const widget = new w.TradingView.widget({
-    // fullscreen: true,
+  return new (window as any).TradingView.widget({
     autosize: true,
     symbol: 'BTCUSD',
     interval: '5',
     container_id: 'tv_chart_container',
     datafeed: new ChartApi(session, config),
     library_path: 'charting_library/',
-    locale: w.getParameterByName('lang') || 'en',
-    // 	Regression Trend-related functionality is not implemented yet, so it's hidden for a while
-    // drawings_access: {type: 'black', tools: [{name: 'Regression Trend'}]},
     disabled_features: ['use_localstorage_for_settings'],
-    // toolbar_bg: '#333333',
     preset: 'mobile',
     overrides: {
+      volumePaneSize: 'tiny',
+
       'paneProperties.background': '#333333',
       'paneProperties.vertGridProperties.color': 'rgba(140, 148, 160, 0.6)',
+      'paneProperties.vertGridProperties.style': LINESTYLE_DOTTED,
       'paneProperties.horzGridProperties.color': 'rgba(140, 148, 160, 0.6)',
+      'paneProperties.horzGridProperties.style': LINESTYLE_DASHED,
       'symbolWatermarkProperties.color': 'rgba(0, 0, 0, 0)',
-      'scalesProperties.textColor': '#AAA',
-      'mainSeriesProperties.candleStyle.drawBorder': true
+
+      'scalesProperties.textColor': 'rgb(140, 148, 160)',
+
+      'mainSeriesProperties.candleStyle.upColor': 'rgb(19, 183, 42)',
+      'mainSeriesProperties.candleStyle.downColor': 'rgb(255, 62, 46)',
+      'mainSeriesProperties.candleStyle.drawWick': true,
+      'mainSeriesProperties.candleStyle.drawBorder': false,
+      'mainSeriesProperties.candleStyle.wickUpColor':
+        'rgba(140, 148, 160, 0.4)',
+      'mainSeriesProperties.candleStyle.wickDownColor':
+        'rgba(140, 148, 160, 0.4)',
+      'mainSeriesProperties.candleStyle.barColorsOnPrevClose': false
     },
     custom_css_url: process.env.PUBLIC_URL + '/chart.css'
   });
-  return widget;
 };
