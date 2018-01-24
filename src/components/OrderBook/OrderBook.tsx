6578a86ff96e0f8cef0f4cb218fb1c559fdde4de
g.moiseychik <g.moiseychik@itransition.com>
2018-01-24 14:06:42 +0300

    feature/LWDEV-5309-set-middle-scroll: improved order book scroll

--- src/components/OrderBook/OrderBook.tsx
+++ src/components/OrderBook/OrderBook.tsx
@@ -1,9 +1,15 @@
 import {defaultTo} from 'rambda';
 import * as React from 'react';
+import Scrollbars from 'react-custom-scrollbars';
 import styled from 'styled-components';
 import {Order} from '../../models';
 import {Table} from '../Table/index';
 import {OrderBookItem} from './';
+
+const Wrapper = styled.div`
+  height: 100%;
+  margin-right: -0.9375rem;
+`;
 
 const StyledHead = styled.thead`
   display: block;
@@ -21,16 +27,17 @@ const StyledRow = styled.tr`
 
 const StyledSellOrders = styled.tbody`
   display: block;
-  margin-top: 34px;
+  margin: 34px 0.9375rem 0 0;
 `;
 
 const StyledBuyOrders = styled(StyledSellOrders)`
-  margin-top: 0;
+  margin: 0 0.9375rem 0 0;
 `;
 
 const StyledMidPrice = styled.tbody`
   display: flex;
   justify-content: center;
+  margin: 0 0.9375rem 0 0;
   background: rgba(0, 0, 0, 0.2);
 `;
 
@@ -45,37 +52,66 @@ interface OrderBookProps {
   mid: number;
 }
 
-const OrderBook: React.SFC<OrderBookProps> = ({
-  asks = [],
-  mid = 0,
-  bids = []
-}) => {
-  return (
-    <div style={{height: '100%'}}>
-      <Table>
-        <StyledHead>
-          <StyledRow>
-            <StyledHeader align="right">Sell</StyledHeader>
-            <StyledHeader align="center">Price</StyledHeader>
-            <StyledHeader align="left">Buy</StyledHeader>
-          </StyledRow>
-        </StyledHead>
-      </Table>
-      <Table>
-        <StyledSellOrders>
-          {asks.map(order => <OrderBookItem key={order.id} {...order} />)}
-        </StyledSellOrders>
-        <StyledMidPrice>
-          <tr>
-            <td>{defaultTo('', Number(mid))}</td>
-          </tr>
-        </StyledMidPrice>
-        <StyledBuyOrders>
-          {bids.map(order => <OrderBookItem key={order.id} {...order} />)}
-        </StyledBuyOrders>
-      </Table>
-    </div>
-  );
-};
+class OrderBook extends React.Component<OrderBookProps> {
+  private scrollComponent: any;
+  private wrapper: any;
+  private content: any;
+
+  private refHandlers = {
+    content: (innerRef: any) => (this.content = innerRef),
+    scrollComponent: (ref: any) => (this.scrollComponent = ref),
+    wrapper: (innerRef: any) => (this.wrapper = innerRef)
+  };
+
+  private isScrollSet: boolean = false;
+
+  componentDidUpdate() {
+    if (this.isScrollSet) {
+      return;
+    }
+
+    const scroll = (this.content.offsetHeight - this.wrapper.offsetHeight) / 2;
+    this.scrollComponent.scrollTop(scroll > 0 ? scroll : 0);
+
+    if (this.props.asks.length && this.props.bids.length && scroll > 0) {
+      this.isScrollSet = true;
+    }
+  }
+
+  render() {
+    return (
+      <Wrapper innerRef={this.refHandlers.wrapper}>
+        <Table>
+          <StyledHead>
+            <StyledRow>
+              <StyledHeader align="right">Sell</StyledHeader>
+              <StyledHeader align="center">Price</StyledHeader>
+              <StyledHeader align="left">Buy</StyledHeader>
+            </StyledRow>
+          </StyledHead>
+        </Table>
+        <Scrollbars autoHide={true} ref={this.refHandlers.scrollComponent}>
+          <Table innerRef={this.refHandlers.content}>
+            <StyledSellOrders>
+              {this.props.asks.map(order => (
+                <OrderBookItem key={order.id} {...order} />
+              ))}
+            </StyledSellOrders>
+            <StyledMidPrice>
+              <tr>
+                <td>{defaultTo('', Number(this.props.mid))}</td>
+              </tr>
+            </StyledMidPrice>
+            <StyledBuyOrders>
+              {this.props.bids.map(order => (
+                <OrderBookItem key={order.id} {...order} />
+              ))}
+            </StyledBuyOrders>
+          </Table>
+        </Scrollbars>
+      </Wrapper>
+    );
+  }
+}
 
 export default OrderBook;
