6c66a7cec27296dc9ffe00e08519b4736abec2e7
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-01-30 22:49:27 +0500

    Use logarithmic scale on orderbook volumes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,6 @@ export {default as StorageUtils} from './storageUtils';
 
 import * as fns from './fn';
 export {fns};
+
+import * as math from './math';
+export {math};
---
+++ src/utils/math.ts
@@ -0,0 +1,1 @@
+export const logBase = (x: number, y: number) => Math.log(y) / Math.log(x);
