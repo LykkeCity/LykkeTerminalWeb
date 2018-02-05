690e5581e8507c37a7264a4feb42d2b24cd9cfe9
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-02-05 17:38:15 +0300

    LWDEV-5413 Account: The asset is displayed if it has a zero balance

--- src/api/__tests__/wampApi.spec.ts
+++ src/api/__tests__/wampApi.spec.ts
@@ -4,7 +4,6 @@ const wamp = new WampApi();
 
 describe('wamp service', () => {
   it('wamp session should be defined after connection established', () => {
-    // TODO: Need to find out why it's impossible to resolve wamp.connect promise here
     expect(wamp.connect).toBeDefined();
     expect(true).toBe(true);
   });
