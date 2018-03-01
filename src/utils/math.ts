190851518acdbc5c0e4c951731688acfdbbb958a
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-03-02 04:02:09 +0500

    Rework trade fetching and mapping

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,1 +1,6 @@
 export const logBase = (x: number, y: number) => Math.log(y) / Math.log(x);
+
+export const precisionRound = (num: number, precision: number) => {
+  const factor = Math.pow(10, precision);
+  return Math.round(num * factor) / factor;
+};
