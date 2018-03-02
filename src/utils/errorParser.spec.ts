b93e5ac2de67920e024ed55008eaae11b785c3b9
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-03-02 16:24:42 +0300

    LWDEV-6069 Human friendly errors are to be displayed - fixed unit tests

--- src/utils/errorParser.spec.ts
+++ src/utils/errorParser.spec.ts
@@ -4,7 +4,7 @@ describe('string utils', () => {
   it('should return splitted string', () => {
     const message = 'me responded: ReservedVolumeHigherThanBalance';
     expect(ErrorParser.getMessage(message)).toBe(
-      'Reserved Volume Higher Than Balance'
+      'reserved volume higher than balance'
     );
   });
 });
