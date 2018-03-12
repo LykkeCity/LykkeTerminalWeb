202e3afd94121a472d46df0f545302f8ca82aedd
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-03-12 17:37:08 +0500

    Rewrite mapping from wamp to trade

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -4,3 +4,13 @@ export const precisionRound = (num: number, precision: number) => {
   const factor = Math.pow(10, precision);
   return Math.round(num * factor) / factor;
 };
+
+export const precisionCeil = (num: number, precision: number) => {
+  const factor = Math.pow(10, precision);
+  return Math.ceil(num * factor) / factor;
+};
+
+export const precisionFloor = (num: number, precision: number) => {
+  const factor = Math.pow(10, precision);
+  return Math.floor(num * factor) / factor;
+};
