421a678f3ec798463197d9cb88012a16ebf2a4f9
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-03-13 14:18:19 +0500

    Refactor orderbook component

---
+++ src/components/Bar/Bar.tsx
@@ -0,0 +1,10 @@
+import {rem} from 'polished';
+import styled from '../styled';
+
+const Bar = styled.div`
+  background-color: rgba(0, 0, 0, 0.2);
+  height: 1px;
+  margin-right: ${rem(15)};
+`;
+
+export default Bar;
