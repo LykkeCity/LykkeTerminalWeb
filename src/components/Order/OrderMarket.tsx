dd2e6e42a4463d644da5b8de1c3e5743ae1bbb37
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-03-14 14:24:51 +0300

    adjust order widget

--- src/components/Order/OrderMarket.tsx
+++ src/components/Order/OrderMarket.tsx
@@ -1,7 +1,133 @@
 import * as React from 'react';
 
-const OrderMarket: React.SFC<{}> = () => {
-  return <h1>Hello</h1>;
+// tslint:disable-next-line:no-var-requires
+const {Flex, Box} = require('grid-styled');
+
+const StyledOrderButton = styled.div`
+  margin-top: ${rem(24)};
+`;
+
+const StyledInvertedBtn = Box.extend`
+  background: url('assets/images/invert.png') no-repeat center;
+  border: 1px solid rgba(140, 148, 160, 0.4);
+  border-radius: 4px;
+  width: 32px;
+  height: 32px;
+  margin: 0 8px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+interface OrderBasicFormState {
+  action: string;
+}
+
+class OrderMarket extends React.Component<
+  OrderBasicFormProps,
+  OrderBasicFormState
+> {
+  private isInverted: boolean = false;
+
+  constructor(props: OrderBasicFormProps) {
+    super(props);
+
+    this.state = {
+      action: this.props.action
+    };
+  }
+
+  onInvert = () => {
+    this.isInverted = !this.isInverted;
+    this.setState({
+      action: !this.isInverted
+        ? this.props.action
+        : this.state.action === orderAction.sell.action
+          ? orderAction.buy.action
+          : orderAction.sell.action
+    });
+  };
+
+  render() {
+    const baseName = this.props.assetName.split('/')[0];
+    const quoteName = this.props.assetName.split('/')[1];
+
+    return (
+      <div>
+        <div>
+          <div>
+            <div style={{width: '75%'}}>
+              <Flex justify="space-between">
+                <StyledActionTitle>
+                  {this.state.action} {!this.isInverted ? baseName : quoteName}
+                </StyledActionTitle>
+                <div>
+                  {this.props.balance}{' '}
+                  {this.props.isSell ? baseName : quoteName} available
+                </div>
+              </Flex>
+            </div>
+          </div>
+          <Flex>
+            <StyledInputControl style={{width: '75%'}}>
+              <NumberInput
+                value={this.props.quantity}
+                id={'quantityValue'}
+                onChange={this.props.onChange(this.props.quantityAccuracy)}
+                onArrowClick={this.props.onArrowClick(
+                  this.props.quantityAccuracy
+                )}
+              />
+            </StyledInputControl>
+            <Flex align={'center'}>
+              <StyledInvertedBtn onClick={this.onInvert} />
+              <Box>{this.isInverted ? baseName : quoteName}</Box>
+            </Flex>
+          </Flex>
+          <div style={{width: '75%'}}>
+            <Flex
+              justify={'space-between'}
+              style={{width: '100%', marginTop: '14px'}}
+            >
+              {this.props.percents!.map((item: any, index: number) => (
+                <OrderPercentage
+                  percent={item.percent}
+                  key={index}
+                  index={index}
+                  onClick={this.props.onHandlePercentageChange(index)}
+                  isActive={item.isActive}
+                />
+              ))}
+            </Flex>
+          </div>
+        </div>
+        <StyledOrderButton>
+          <OrderButton
+            action={this.props.action}
+            isDisable={this.props.isDisable}
+            type={'submit'}
+            message={`${capitalize(this.props.action)} ${
+              this.props.quantity
+            } ${baseName}`}
+          />
+        </StyledOrderButton>
+        <StyledReset justify={'center'}>
+          <span onClick={this.props.onReset}>Reset and clear</span>
+        </StyledReset>
+      </div>
+    );
+  }
+}
+
+const OrderMarketForm: React.SFC<OrderBasicFormProps> = (
+  props: OrderBasicFormProps
+) => {
+  return (
+    <Form>
+      <OrderMarket {...props} />
+    </Form>
+  );
 };
 
 export default OrderMarket;
