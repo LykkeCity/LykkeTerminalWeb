622599ba8941eb408ce9902bf74b5c1faedf0b2c
ArtLait <pozdnyakovlait@gmail.com>
2018-03-16 19:16:42 +0300

    change change from date-fns to react-intl and change FormattedTime in PublicTradeListItem component

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,25 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import {RootStore} from './stores/index';
+import {addLocaleData, IntlProvider} from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import ru from 'react-intl/locale-data/ru';
+
+addLocaleData([...en, ...ru]);
+
+let locale =
+  (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  'en-US';
 
 const rootStore = new RootStore();
 
 const render = (AppComponent: any) => {
   ReactDOM.render(
     <Provider {...rootStore}>
-      <AppComponent />
+      <IntlProvider>
+        <AppComponent locale={locale} />
+      </IntlProvider>
     </Provider>,
     document.getElementById('root') as HTMLElement
   );
