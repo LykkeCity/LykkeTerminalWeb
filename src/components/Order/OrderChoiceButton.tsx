4e83aace006b6a541d5d751787206af3d57d8611
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-03-22 04:37:05 +0500

    Adjust styles

--- src/components/Order/OrderChoiceButton.tsx
+++ src/components/Order/OrderChoiceButton.tsx
@@ -5,15 +5,10 @@ import {OrderChoiceButtonProps} from './index';
 
 const StyledColumn = styled.div`
   display: flex;
-  flex-direction: row;
   align-items: center;
 
-  &:first-child {
-    padding-right: ${rem(4)};
-  }
-
-  &:not(:first-child):last-child {
-    padding-left: ${rem(4)};
+  &:not(:last-child) {
+    margin-right: ${rem(22)};
   }
 
   margin-bottom: -1px;
@@ -23,13 +18,14 @@ const StyledActionChoice = styled.div`
   cursor: pointer;
   text-align: center;
   padding: ${rem(16)} 0;
-  color: #8c94a0;
+  color: rgb(140, 148, 160);
   font-size: ${rem(18)};
   border-bottom: 1px solid transparent;
 
   &.active {
-    border-bottom: 1px solid #0388ef;
-    color: #ffffff;
+    border-bottom: 2px solid rgb(3, 136, 239);
+    color: rgb(255, 255, 255);
+    font-weight: 600;
   }
 `;
 
