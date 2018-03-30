7af65a65ff548544c6df14742f4849125f3d0a8f
s.shchyogolev <s.shchyogolev@itransition.com>
2018-03-30 13:28:30 +0300

    add reset method

--- src/api/chartApi.ts
+++ src/api/chartApi.ts
@@ -4,18 +4,21 @@ import {RestApi} from './restApi';
 export interface ChartApi {
   save: (body: any, key: string) => Promise<any>;
   load: (key: string) => Promise<any>;
+  reset: () => Promise<any>;
 }
 
 export class RestChartApi extends RestApi implements ChartApi {
   save = (body: any) =>
     this.fireAndForget(`/dictionary/${keys.chartKey}`, body);
   load = () => this.get(`/dictionary/${keys.chartKey}`);
+  reset = () => this.delete(`/dictionary/${keys.chartKey}`);
 }
 
 // tslint:disable-next-line:max-classes-per-file
 export class MockChartApi implements ChartApi {
   load = () => Promise.resolve([]);
   save = (body: any) => Promise.resolve([]);
+  reset = () => Promise.resolve();
 }
 
 export default RestChartApi;
