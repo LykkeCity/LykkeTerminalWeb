0a738213e20baea9f78cb93c5d572cdf615a4949
ArtLait <pozdnyakovlait@gmail.com>
2018-04-02 19:11:43 +0300

    fix bug with empty asset

--- src/components/WalletBalanceList/WalletBalanceItem.tsx
+++ src/components/WalletBalanceList/WalletBalanceItem.tsx
@@ -1,36 +1,49 @@
 import * as React from 'react';
 import {AssetModel} from '../../models';
 import WalletBalanceNumber from './WalletBalanceNumber';
-
 interface WalletBalanceItemProps {
   balance: any;
   baseAsset: AssetModel;
   asset: AssetModel;
 }
-
 const WalletBalanceItem: React.SFC<WalletBalanceItemProps> = ({
   balance,
   baseAsset,
   asset
-}) => (
-  <tr>
-    <td>{asset.name}</td>
-    <td />
-    <td>
-      <WalletBalanceNumber
-        num={balance.balanceInBaseAsset}
-        accuracy={baseAsset.accuracy}
-        color={'rgba(245, 246, 247, 0.4)'}
-      >
-        &nbsp;{baseAsset.name}
-      </WalletBalanceNumber>
-    </td>
-    <td>
-      <WalletBalanceNumber num={balance.Balance} accuracy={asset.accuracy}>
-        &nbsp;{asset.name}
-      </WalletBalanceNumber>
-    </td>
-  </tr>
-);
-
+}) =>
+  (asset && (
+    <tr>
+      <td>{asset.name}</td>
+      <td />
+      <td>
+        <WalletBalanceNumber
+          num={balance.balanceInBaseAsset}
+          accuracy={baseAsset.accuracy}
+          color={'rgba(245, 246, 247, 0.4)'}
+        >
+          &nbsp;{baseAsset.name}
+        </WalletBalanceNumber>
+      </td>
+      <td>
+        <WalletBalanceNumber num={balance.Balance} accuracy={asset.accuracy}>
+          &nbsp;{asset.name}
+        </WalletBalanceNumber>
+      </td>
+    </tr>
+  )) || (
+    <tr>
+      <td />
+      <td />
+      <td>
+        <WalletBalanceNumber
+          num={balance.balanceInBaseAsset}
+          accuracy={baseAsset.accuracy}
+          color={'rgba(245, 246, 247, 0.4)'}
+        >
+          &nbsp;{baseAsset.name}
+        </WalletBalanceNumber>
+      </td>
+      <td />
+    </tr>
+  );
 export default WalletBalanceItem;
