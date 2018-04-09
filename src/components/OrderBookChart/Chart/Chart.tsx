69b95e8a6645f53f5455f5feafe798d1616ffdfb
GeorgePuisha <e.puysha@itransition.com>
2018-04-09 18:10:13 +0300

    Add library to detect resize

--- src/components/OrderBookChart/Chart/Chart.tsx
+++ src/components/OrderBookChart/Chart/Chart.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
+import {observable} from 'mobx';
+
 import {FastLayer, Layer, Stage} from 'react-konva';
+
+import Measure from 'react-measure';
 
 import Asks from './Asks';
 import Bids from './Bids';
@@ -11,8 +15,8 @@ import {ChartProps} from './Models';
 import chart from './chartConstants';
 
 class Chart extends React.Component<ChartProps> {
-  width: number = chart.width;
-  height: number = chart.height;
+  @observable width: number;
+  @observable height: number;
 
   constructor(props: ChartProps) {
     super(props);
@@ -22,33 +26,47 @@ class Chart extends React.Component<ChartProps> {
     const {asks, bids, mid, baseAsset, quoteAsset} = this.props;
 
     return (
-      <Stage width={this.width} height={this.height}>
-        <FastLayer clearBeforeDraw={true}>
-          <Mesh
-            asks={asks}
-            bids={bids}
-            mid={mid}
-            baseAsset={baseAsset}
-            quoteAsset={quoteAsset}
-          />
-        </FastLayer>
-        <Layer>
-          <Asks
-            asks={asks}
-            bids={bids}
-            mid={mid}
-            baseAsset={baseAsset}
-            quoteAsset={quoteAsset}
-          />
-          <Bids
-            asks={asks}
-            bids={bids}
-            mid={mid}
-            baseAsset={baseAsset}
-            quoteAsset={quoteAsset}
-          />
-        </Layer>
-      </Stage>
+      <Measure
+        // tslint:disable-next-line:jsx-boolean-value
+        bounds
+        // tslint:disable-next-line:jsx-no-lambda
+        onResize={contentRect => {
+          this.width = contentRect.bounds!.width;
+          this.height = chart.height;
+        }}
+      >
+        {({measureRef}) => (
+          <div ref={measureRef}>
+            <Stage width={this.width} height={this.height}>
+              <FastLayer clearBeforeDraw={true}>
+                <Mesh
+                  asks={asks}
+                  bids={bids}
+                  mid={mid}
+                  baseAsset={baseAsset}
+                  quoteAsset={quoteAsset}
+                />
+              </FastLayer>
+              <Layer>
+                <Asks
+                  asks={asks}
+                  bids={bids}
+                  mid={mid}
+                  baseAsset={baseAsset}
+                  quoteAsset={quoteAsset}
+                />
+                <Bids
+                  asks={asks}
+                  bids={bids}
+                  mid={mid}
+                  baseAsset={baseAsset}
+                  quoteAsset={quoteAsset}
+                />
+              </Layer>
+            </Stage>
+          </div>
+        )}
+      </Measure>
     );
   }
 }
