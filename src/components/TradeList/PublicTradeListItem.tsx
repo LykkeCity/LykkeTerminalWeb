afd512eb00ec98bece39840144d51b19f8eb8971
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-04-10 02:43:19 +0500

    LWDEV-6791: Fetch daily candle separately
    Display trade time for today's trades and full date in the title and for the rest trades

--- src/components/TradeList/PublicTradeListItem.tsx
+++ src/components/TradeList/PublicTradeListItem.tsx
@@ -1,8 +1,8 @@
+import {isToday} from 'date-fns';
 import * as React from 'react';
-import {PublicTradesCellWidth} from '.';
 import {TradeModel} from '../../models/index';
-import {toLocaleStringWithAccuracy} from '../../utils/string';
 import {SideCell} from './styles';
+import {toLocaleStringWithAccuracy} from './TradeListItem';
 
 // tslint:disable-next-line:no-empty-interface
 export interface PublicTradeListItemProps extends TradeModel {}
@@ -13,17 +13,22 @@ export const PublicTradeListItem: React.SFC<PublicTradeListItemProps> = ({
   side,
   instrument,
   timestamp
-}) => (
-  <tr>
-    <td>
-      {toLocaleStringWithAccuracy(volume, instrument!.baseAsset.accuracy)}
-    </td>
-    <td>{toLocaleStringWithAccuracy(price, instrument!.accuracy)}</td>
-    <SideCell w={PublicTradesCellWidth.Side} side={side}>
-      {side}
-    </SideCell>
-    <td>{new Date(timestamp).toLocaleTimeString()}</td>
-  </tr>
-);
+}) => {
+  const date = new Date(timestamp);
+  return (
+    <tr>
+      <td>
+        {toLocaleStringWithAccuracy(volume, instrument!.baseAsset.accuracy)}
+      </td>
+      <td>{toLocaleStringWithAccuracy(price, instrument!.accuracy)}</td>
+      <SideCell w={50} side={side}>
+        {side}
+      </SideCell>
+      <td title={date.toLocaleString()}>
+        {isToday(date) ? date.toLocaleTimeString() : date.toLocaleString()}
+      </td>
+    </tr>
+  );
+};
 
 export default PublicTradeListItem;
