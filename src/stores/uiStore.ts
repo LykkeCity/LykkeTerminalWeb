e3e7d9aa7aa69dc7b3ee0395c9a93e1165486ce9
g.moiseychik <g.moiseychik@itransition.com>
2018-04-18 16:17:37 +0300

    feature_LWDEV-6837-hide-performance-data: hide instrument performance data when changing instrument

--- src/stores/uiStore.ts
+++ src/stores/uiStore.ts
@@ -15,6 +15,7 @@ class UiStore extends BaseStore {
   @observable searchWalletName: string = Watchlists.All;
   @observable selectedInstrument: InstrumentModel | null;
   @observable showInstrumentPicker = false;
+  @observable showInstrumentPerformanceData = false;
   @observable showOrdersSelect: boolean = false;
   stateFns: any = [];
   initPriceUpdate: any;
@@ -25,6 +26,8 @@ class UiStore extends BaseStore {
       () => this.selectedInstrument,
       async instrument => {
         if (instrument) {
+          this.toggleInstrumentPerformanceData(false);
+
           const {reset, fetchAll, subscribe} = this.rootStore.orderBookStore;
           reset();
           await fetchAll(); // should be waited for loading bids and asks
@@ -63,6 +66,8 @@ class UiStore extends BaseStore {
           await fetchLastPrice();
           await fetchDailyCandle();
           subscribeToDailyCandle();
+
+          this.toggleInstrumentPerformanceData(true);
         }
       }
     );
@@ -95,6 +100,10 @@ class UiStore extends BaseStore {
   @action
   toggleInstrumentPicker = () =>
     (this.showInstrumentPicker = !this.showInstrumentPicker);
+
+  @action
+  toggleInstrumentPerformanceData = (show: boolean) =>
+    (this.showInstrumentPerformanceData = show);
 
   reset = () => {
     this.searchTerm = '';
