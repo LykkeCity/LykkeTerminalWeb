1920588b34bbf000a6faabbe37410b6d6824b9c1
ArtLait <pozdnyakovlait@gmail.com>
2018-04-19 22:57:53 +0300

    added correct resizing chart

---
+++ src/components/Chart/styles.ts
@@ -0,0 +1,18 @@
+import styled from '../styled';
+
+export const ChartWrapper = styled.div`
+  height: 100%;
+  position: relative;
+`;
+
+export const ChartContainer = styled.div`
+  height: 100%;
+`;
+
+export const TransparentDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  display: none;
+`;
