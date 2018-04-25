a1240860a115071f39d34a482ff46d1e90ce02a0
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-04-25 11:42:49 +0300

    fixed layout storage key

--- src/models/storageKeys.ts
+++ src/models/storageKeys.ts
@@ -2,7 +2,7 @@ enum keys {
   baseAsset = 'lwt-baseAsset',
   chartKey = 'lwt-chart-settings',
   confirmReminder = 'lwt-confirmReminder',
-  layout = 'lwt-layout',
+  layout = 'lwt-layout-2',
   notificationId = 'lwt-notificationId',
   selectedInstrument = 'selected-instrument',
   state = 'lwt-state',
