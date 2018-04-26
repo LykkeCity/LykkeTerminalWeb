8c62e210dfb730aa6b57fec57840a460e13e7d16
g.moiseychik <g.moiseychik@itransition.com>
2018-04-26 16:08:33 +0300

    bugfix_LWDEV-7090-firefox-timezone-detection-bug: updated timezone detection logic

--- src/utils/dateFns.ts
+++ src/utils/dateFns.ts
@@ -69,8 +69,16 @@ export const splitter = (from: number, to: number, resolution: string) => {
 };
 
 export const getTimeZone = (zones: any[]) => {
-  const gmt = new Date().toString().match(/([-\+][0-9]+)/)![1];
   const zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const gmt = (() => {
+    const offset = new Date().getTimezoneOffset();
+    const absOffset = Math.abs(offset);
+    return (
+      (offset < 0 ? '+' : '-') +
+      ('00' + Math.floor(absOffset / 60)).slice(-2) +
+      ('00' + absOffset % 60).slice(-2)
+    );
+  })();
 
   let timezone =
     zones.find((z: any) => z.zone === zone) ||
