03277cfcdf3ea130222f9ccf14c458459118ccc2
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-05-04 15:36:46 +0300

    added tests

--- src/utils/localFormatted/localFormatted.spec.ts
+++ src/utils/localFormatted/localFormatted.spec.ts
@@ -1,4 +1,7 @@
-import {checkForTrailingZero} from './localFormatted';
+import {
+  checkForTrailingZero,
+  default as formattedNumber
+} from './localFormatted';
 
 describe('Test locale for number and date', () => {
   it('should return string number with locale', () => {
@@ -11,5 +14,15 @@ describe('Test locale for number and date', () => {
     expect(checkForTrailingZero('50000.050004')).toBe('50000.050004');
     expect(checkForTrailingZero('50000.0000')).toBe('50000.00');
     expect(checkForTrailingZero('50000.1000')).toBe('50000.10');
+  });
+
+  it('should return pure value', () => {
+    expect(checkForTrailingZero('1.234')).toBe('1.234');
+  });
+
+  it('should return value with right quantity of trailing zeroes', () => {
+    expect(formattedNumber(5, 5)).toBe('5.00');
+    expect(formattedNumber(5.234, 5)).toBe('5.2340');
+    expect(formattedNumber(5.23455, 5)).toBe('5.23455');
   });
 });
