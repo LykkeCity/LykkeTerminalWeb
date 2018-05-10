eba7ba1ed8a0c2a704c0379d7f1a35528ccba341
GeorgePuisha <e.puysha@itransition.com>
2018-05-10 14:29:07 +0300

    Connect components to a store

--- src/components/DepthChart/Chart/index.tsx
+++ src/components/DepthChart/Chart/index.tsx
@@ -1,20 +1,63 @@
 import {connect} from '../../connect';
+import Chart from './Chart';
 import ChartWrapper from './ChartWrapper';
+import Mesh from './Mesh';
+import Pointer from './Pointer';
 
 // tslint:disable:object-literal-sort-keys
-const ConnectedChart = connect(
-  ({depthChartStore: {asks, bids, mid}, uiStore: {selectedInstrument}}) => {
-    const midPrice = mid();
-
+const ConnectedChartWrapper: any = connect(
+  ({uiStore: {selectedInstrument}}) => {
     return {
-      asks,
-      bids,
-      mid: midPrice,
       selectedInstrument
     };
   },
   ChartWrapper
 );
 
-export default ConnectedChart;
+const ConnectedMesh = connect(
+  ({depthChartStore: {asks, bids, mid}, uiStore: {selectedInstrument}}) => {
+    return {
+      asks,
+      bids,
+      mid: mid(),
+      baseAsset: selectedInstrument!.baseAsset.name,
+      quoteAsset: selectedInstrument!.quoteAsset.name,
+      quoteAccuracy: selectedInstrument!.quoteAsset.accuracy,
+      baseAccuracy: selectedInstrument!.baseAsset.accuracy,
+      priceAccuracy: selectedInstrument!.accuracy
+    };
+  },
+  Mesh
+);
+
+const ConnectedChart = connect(
+  ({depthChartStore: {asks, bids, mid}, uiStore: {selectedInstrument}}) => {
+    return {
+      asks,
+      bids,
+      mid: mid(),
+      baseAsset: selectedInstrument!.baseAsset.name,
+      quoteAsset: selectedInstrument!.quoteAsset.name,
+      quoteAccuracy: selectedInstrument!.quoteAsset.accuracy,
+      baseAccuracy: selectedInstrument!.baseAsset.accuracy,
+      priceAccuracy: selectedInstrument!.accuracy
+    };
+  },
+  Chart
+);
+
+const ConnectedPointer = connect(({uiStore: {selectedInstrument}}) => {
+  return {
+    baseAsset: selectedInstrument!.baseAsset.name,
+    quoteAsset: selectedInstrument!.quoteAsset.name,
+    quoteAccuracy: selectedInstrument!.quoteAsset.accuracy,
+    baseAccuracy: selectedInstrument!.baseAsset.accuracy,
+    priceAccuracy: selectedInstrument!.accuracy
+  };
+}, Pointer);
+
+export default ConnectedChartWrapper;
 export {default as ChartWrapper} from './ChartWrapper';
+export {ConnectedMesh as Mesh};
+export {ConnectedChart as Chart};
+export {ConnectedPointer as Pointer};
