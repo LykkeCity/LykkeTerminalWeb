e80d528b41da12ccf2a70894187995da6de30d8c
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-06-26 17:43:19 +0300

    LWDEV-8102 App crashes on Safari and Edge

--- src/workers/worker.ts
+++ src/workers/worker.ts
@@ -1,2 +1,15 @@
+import greenlet from 'greenlet';
+import {Browser} from '../models';
+
 export const workerMock = (cb: any) => (...args: any[]) =>
   Promise.resolve(cb(...args));
+
+export const getWorkerByCurrentBrowser = (browser: string) => {
+  switch (browser) {
+    case Browser.Chrome:
+    case Browser.Firefox:
+      return greenlet;
+    default:
+      return workerMock;
+  }
+};
