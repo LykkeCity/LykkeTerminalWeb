a6957efab9a37f0c0a489e78f7e00f7dba58c1f9
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-06-26 18:19:28 +0300

    Update code in keeping with PR
    New test was added. Changed method's name into more convenient.

--- src/components/OrderBook/LevelList.tsx
+++ src/components/OrderBook/LevelList.tsx
@@ -20,7 +20,7 @@ import {
 import LevelType from '../../models/levelType';
 import OrderBookCellType from '../../models/orderBookCellType';
 import {
-  getTrailingZeroStartPosition,
+  getTrailingZeroOppositePosition,
   hasTrailingZeroes
 } from '../../utils/string';
 import {FakeOrderBookStage} from './styles';
@@ -289,7 +289,7 @@ class LevelList extends React.Component<LevelListProps> {
 
       if (hasTrailingZeroes(volume)) {
         let drownSymbolsWidth = 0;
-        const trailingZeroPosition = getTrailingZeroStartPosition(volume);
+        const trailingZeroPosition = getTrailingZeroOppositePosition(volume);
 
         volume.split('').forEach((symbol: string, index: number) => {
           const symbolColor =
