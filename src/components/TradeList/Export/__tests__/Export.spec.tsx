dfe5945cdd63f9fd89788c5b416789e3c3343aae
GeorgePuisha <e.puysha@itransition.com>
2018-07-05 16:18:44 +0300

    Add tests for Export button and mappers

---
+++ src/components/TradeList/Export/__tests__/Export.spec.tsx
@@ -0,0 +1,30 @@
+import {mount, render} from 'enzyme';
+import React from 'react';
+import Export from '../Export';
+
+describe('<Export>', () => {
+  const fetchHistory = jest.fn();
+  let canExport = jest.fn(() => true);
+
+  // tslint:disable-next-line:no-shadowed-variable
+  const getExportButton = (canExport: () => boolean) => {
+    return <Export canExport={canExport} fetchHistory={fetchHistory} />;
+  };
+
+  it('should render content', () => {
+    const wrapper = mount(getExportButton(canExport));
+    expect(wrapper.find('Export')).toHaveLength(1);
+  });
+
+  it('should be disabled if cannot export', () => {
+    canExport = jest.fn(() => false);
+    const wrapper = render(getExportButton(canExport));
+    expect(wrapper.find('Export').hasClass('clickable')).toBeFalsy();
+  });
+
+  it('should have proper title', () => {
+    const title = 'Export history (csv)';
+    const wrapper = mount(getExportButton(canExport));
+    expect(wrapper.find('Export').text()).toEqual(title);
+  });
+});
--- src/components/TradeList/Export/exportDataAndSave.ts
+++ src/components/TradeList/Export/exportDataAndSave.ts
@@ -1,15 +1,8 @@
 import {HistoryResponseModel} from '../../../models';
+import {historyToCsv} from '../../../models/mappers/historyToCsv';
 
-const Types = {
-  Buy: 'Buy',
-  Sell: 'Sell'
-};
-
-const exchange = 'Lykke';
 const extension = '.csv';
 const documentType = 'text/csv;charset=utf-8;';
-const csvHeader =
-  'Date\tType\tExchange\tBase amount\tBase currency\tQuote amount\tQuote currency\tFee\tFee currency\n';
 
 const downloadLink = document.createElement('a');
 downloadLink.style.display = 'none';
@@ -18,47 +11,9 @@ downloadLink.onclick = (event: MouseEvent) => {
   document.body.removeChild(event.target as Node);
 };
 
-const generateId = (): number => new Date().getTime();
-
-const parseType = (record: HistoryResponseModel): string => {
-  if (record.AssetPair) {
-    return record.Amount! < 0 ? Types.Sell : Types.Buy;
-  } else {
-    return record.Type;
-  }
-};
-
-const parsePriceAsset = (assetPair: string, asset: string) => {
-  if (assetPair && asset) {
-    return assetPair.replace(asset, '');
-  }
-  return '';
-};
-
-const generateCsvRow = (record: HistoryResponseModel): string => {
-  const isZeroFee = record.FeeSize === 0;
-  const type = parseType(record);
-  const priceAsset = parsePriceAsset(record.AssetPair!, record.Asset!);
-  const row = [
-    record.DateTime,
-    type,
-    exchange,
-    Math.abs(record.Amount),
-    record.Asset,
-    record.Price,
-    priceAsset,
-    isZeroFee ? '' : record.FeeSize,
-    isZeroFee ? '' : record.Asset
-  ];
-  return row.join('\t') + '\r\n';
-};
-
-const mapToCsv = (rawData: HistoryResponseModel[]): string => {
-  let csv = csvHeader;
-  rawData.forEach(record => {
-    csv += generateCsvRow(record);
-  });
-  return csv;
+const generateName = (): string => {
+  const id = new Date().getTime();
+  return `trades-${id}${extension}`;
 };
 
 const downloadFile = (objectUrl: string, filename: string) => {
@@ -71,9 +26,9 @@ const downloadFile = (objectUrl: string, filename: string) => {
 const saveFile = async (
   fetchHistory: () => Promise<HistoryResponseModel[]>
 ) => {
-  const rawData = await fetchHistory();
-  const filename = `trades-${generateId()}${extension}`;
-  const dataToSave = new Blob([mapToCsv(rawData)], {type: documentType});
+  const rawHistory = await fetchHistory();
+  const filename = generateName();
+  const dataToSave = new Blob([historyToCsv(rawHistory)], {type: documentType});
   if (navigator.msSaveBlob) {
     navigator.msSaveBlob(dataToSave, filename);
   } else {
