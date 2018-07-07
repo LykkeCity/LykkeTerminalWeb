2cbf5a2c3468cbce738c9d7858caec5024e11dbd
Paul Rumyantsev <40331483+PaulRumyantsevItr@users.noreply.github.com>
2018-07-07 18:42:05 +0300

    Remove manual sorting in the trade log (#292)
    Resolves LWDEV-8028

--- src/components/TradeList/TradeLog.tsx
+++ src/components/TradeList/TradeLog.tsx
@@ -1,86 +1,39 @@
 import {pathOr} from 'rambda';
 import * as React from 'react';
-import {InstrumentModel, SortDirection, TradeModel} from '../../models';
+import {InstrumentModel, TradeModel} from '../../models';
 import {LoaderProps} from '../Loader/withLoader';
-import {
-  checkDataForSorting,
-  sortData,
-  TableHeader,
-  TableSortState
-} from '../Table';
-import {PublicTradeList, PublicTradesCellWidth} from './';
+import {HeaderProps, TableHeaderWithoutSort} from '../Table';
+import {PublicTradeList} from './';
 
 export interface TradeLogProps extends LoaderProps {
   selectedInstrument: InstrumentModel;
   trades: TradeModel[];
 }
 
-class TradeLog extends React.Component<TradeLogProps, TableSortState> {
-  constructor(props: TradeLogProps) {
-    super(props);
-    this.state = {
-      data: this.props.trades,
-      sortByParam: '',
-      sortDirection: SortDirection.ASC
-    };
-  }
+const TradeLog: React.SFC<TradeLogProps> = ({selectedInstrument, trades}) => {
+  const headers: HeaderProps[] = [
+    {
+      key: 'price',
+      value: `Price (${pathOr('', ['quoteAsset', 'name'], selectedInstrument)})`
+    },
+    {
+      className: 'right-align',
+      key: 'volume',
+      value: 'Trade size'
+    },
+    {
+      className: 'right-align',
+      key: 'timestamp',
+      value: 'Time'
+    }
+  ];
 
-  componentWillReceiveProps(args: any) {
-    this.setState({
-      data: args.trades
-    });
-  }
-
-  sort = (sortByParam: string, sortDirectionDefault: string) => {
-    this.setState(
-      sortData(this.props.trades, sortByParam, sortDirectionDefault, this.state)
-    );
-  };
-
-  render() {
-    const headers: any[] = [
-      {
-        sortDisabled: checkDataForSorting(this.state.data, 'price'),
-        key: 'price',
-        value: `Price (${pathOr(
-          '',
-          ['quoteAsset', 'name'],
-          this.props.selectedInstrument
-        )})`
-      },
-      {
-        sortDisabled: checkDataForSorting(this.state.data, 'volume'),
-        className: 'right-align',
-        key: 'volume',
-        value: 'Trade size'
-      },
-      {
-        sortDisabled: checkDataForSorting(this.state.data, 'side'),
-        className: 'right-align',
-        key: 'side',
-        value: 'Side',
-        width: PublicTradesCellWidth.Side
-      },
-      {
-        sortDisabled: checkDataForSorting(this.state.data, 'timestamp'),
-        className: 'right-align',
-        key: 'timestamp',
-        value: 'Time'
-      }
-    ];
-
-    return (
-      <React.Fragment>
-        <TableHeader
-          currentSortDirection={this.state.sortDirection}
-          currentSortByParam={this.state.sortByParam}
-          headers={headers}
-          onSort={this.sort}
-        />
-        <PublicTradeList trades={this.state.data} />
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <TableHeaderWithoutSort headers={headers} />
+      <PublicTradeList trades={trades} />
+    </React.Fragment>
+  );
+};
 
 export default TradeLog;
