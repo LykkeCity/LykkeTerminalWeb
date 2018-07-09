eb7ceabc086b7bbeaa7744652d1b29370d703e55
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-07-09 12:53:24 +0300

    Fix wamp's session issue during switching instruments

--- src/stores/uiStore.ts
+++ src/stores/uiStore.ts
@@ -1,4 +1,4 @@
-import {action, computed, observable, reaction} from 'mobx';
+import {action, computed, observable} from 'mobx';
 import {pathOr} from 'rambda';
 import {keys} from '../models';
 import {
@@ -7,9 +7,9 @@ import {
   TradeFilter
 } from '../models/index';
 import Watchlists from '../models/watchlists';
-import {fns, StorageUtils} from '../utils/index';
+import {StorageUtils} from '../utils/index';
 import {DEFAULT_INPUT_VALUE} from '../utils/inputNumber';
-import {BaseStore, RootStore} from './index';
+import {BaseStore} from './index';
 
 const instrumentStorage = StorageUtils(keys.selectedInstrument);
 
@@ -34,75 +34,6 @@ class UiStore extends BaseStore {
   @observable isDisclaimerShown: boolean = false;
   @observable private isReadOnlyMode: boolean;
   private isPageVisible: boolean = true;
-
-  constructor(store: RootStore) {
-    super(store);
-    reaction(
-      () => this.selectedInstrument,
-      async instrument => {
-        if (instrument) {
-          this.toggleInstrumentPerformanceData(false);
-
-          const {reset, fetchAll, subscribe} = this.rootStore.orderBookStore;
-          reset();
-
-          try {
-            await fetchAll(); // should be waited for loading bids and asks
-          } catch (e) {
-            return;
-          }
-
-          subscribe(this.getWs());
-
-          const {
-            setQuantityAccuracy,
-            setPriceAccuracy,
-            setPriceValueWithFixed,
-            setQuantityValue
-          } = this.rootStore.uiOrderStore;
-          setPriceAccuracy(pathOr(2, ['accuracy'], instrument));
-          setQuantityAccuracy(pathOr(2, ['baseAsset', 'accuracy'], instrument));
-          const mid = await this.rootStore.orderBookStore.mid();
-          setPriceValueWithFixed(mid);
-          setQuantityValue(DEFAULT_INPUT_VALUE);
-
-          const {
-            resetTrades,
-            fetchTrades,
-            resetPublicTrades,
-            fetchPublicTrades,
-            subscribeToPublicTrades
-          } = this.rootStore.tradeStore;
-
-          if (
-            this.rootStore.tradeStore.filter === TradeFilter.CurrentAsset &&
-            this.rootStore.authStore.isAuth
-          ) {
-            fns.seq(resetTrades, fetchTrades)();
-          }
-
-          fns.seq(
-            resetPublicTrades,
-            fetchPublicTrades,
-            subscribeToPublicTrades
-          )();
-
-          const {
-            fetchLastPrice,
-            fetchDailyCandle,
-            subscribeToDailyCandle,
-            reset: resetPriceStore
-          } = this.rootStore.priceStore;
-          resetPriceStore();
-          await fetchLastPrice();
-          await fetchDailyCandle();
-          subscribeToDailyCandle();
-
-          this.toggleInstrumentPerformanceData(true);
-        }
-      }
-    );
-  }
 
   setPageVisibility = (isVisible: boolean) => (this.isPageVisible = isVisible);
   getPageVisibility = () => this.isPageVisible;
@@ -132,8 +63,73 @@ class UiStore extends BaseStore {
     const selectedInstrument = getInstrumentById(id);
     instrumentStorage.set(JSON.stringify(selectedInstrument));
     this.selectedInstrument = selectedInstrument!;
+    await this.updateAppData();
     await this.rootStore.chartStore.renderChart();
     this.isDisclaimerShown = this.hasAsset(selectedInstrument!, 'EOS');
+  };
+
+  updateAppData = async () => {
+    if (this.selectedInstrument) {
+      this.toggleInstrumentPerformanceData(false);
+
+      const {reset, fetchAll, subscribe} = this.rootStore.orderBookStore;
+      reset();
+
+      try {
+        await fetchAll(); // should be waited for loading bids and asks
+      } catch (e) {
+        return;
+      }
+
+      subscribe(this.getWs());
+
+      const {
+        setQuantityAccuracy,
+        setPriceAccuracy,
+        setPriceValueWithFixed,
+        setQuantityValue
+      } = this.rootStore.uiOrderStore;
+      setPriceAccuracy(pathOr(2, ['accuracy'], this.selectedInstrument));
+      setQuantityAccuracy(
+        pathOr(2, ['baseAsset', 'accuracy'], this.selectedInstrument)
+      );
+      const mid = await this.rootStore.orderBookStore.mid();
+      setPriceValueWithFixed(mid);
+      setQuantityValue(DEFAULT_INPUT_VALUE);
+
+      const {
+        resetTrades,
+        fetchTrades,
+        resetPublicTrades,
+        fetchPublicTrades,
+        subscribeToPublicTrades
+      } = this.rootStore.tradeStore;
+
+      if (
+        this.rootStore.tradeStore.filter === TradeFilter.CurrentAsset &&
+        this.rootStore.authStore.isAuth
+      ) {
+        resetTrades();
+        await fetchTrades();
+      }
+
+      resetPublicTrades();
+      await fetchPublicTrades();
+      await subscribeToPublicTrades();
+
+      const {
+        fetchLastPrice,
+        fetchDailyCandle,
+        subscribeToDailyCandle,
+        reset: resetPriceStore
+      } = this.rootStore.priceStore;
+      resetPriceStore();
+      await fetchLastPrice();
+      await fetchDailyCandle();
+      await subscribeToDailyCandle();
+
+      this.toggleInstrumentPerformanceData(true);
+    }
   };
 
   @action search = (term: string) => (this.searchTerm = term);
