55230191166d2cfad0a636675952b80da0e5d9b3
Paul Rumyantsev <paul.rumyantsev@wrenkitchens.com>
2018-07-16 11:23:16 +0300

    LWDEV-7718: Fix React warnings in tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,8 @@
+import 'raf/polyfill';
+(window as any).requestAnimationFrame = callback => setTimeout(callback, 0);
+
 import Enzyme, {mount, render, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import 'raf/polyfill';
-
-(window as any).requestAnimationFrame = callback => setTimeout(callback, 0);
 
 // React 16 Enzyme adapter
 Enzyme.configure({adapter: new Adapter()});
