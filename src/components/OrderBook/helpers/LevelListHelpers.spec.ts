ffae4f4f18eded82938079e85f38d8b3ff4177f0
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-08-14 12:26:19 +0300

    Changes due to PR's comments

--- src/components/OrderBook/helpers/LevelListHelpers.spec.ts
+++ src/components/OrderBook/helpers/LevelListHelpers.spec.ts
@@ -34,7 +34,7 @@ describe('level list helpers', () => {
     const bidIndex = 1;
     const side = Side.Buy;
     const levelHeight = 30;
-    const yBidPosition = bidIndex * levelHeight;
+    const yBidPosition = 30;
     expect(getY(side, bidIndex, levelHeight)).toBe(yBidPosition);
   });
 
