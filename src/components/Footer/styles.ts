1fbaf5f370a39462b51da362b487cc0229c38d17
Paul Rumyantsev <p.rumyantsev@itransition.com>
2018-08-20 10:52:19 +0300

    LWDEV-8967: Update header

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,7 +4,7 @@ import styled, {fonts} from '../styled';
 // tslint:disable-next-line:no-var-requires
 const {Box} = require('grid-styled');
 
-export const FooterWrapper = styled.header`
+export const FooterWrapper = styled.footer`
   padding: ${rem(9)} ${rem(11)};
   height: 27px;
   border-bottom: solid 1px #292929;
