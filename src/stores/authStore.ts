bc93416487b4d74d023f5786c3deb44a0969b105
GeorgePuisha <e.puysha@itransition.com>
2018-08-27 17:24:15 +0300

    Build callback url from origin & auth route

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -92,14 +92,12 @@ class AuthStore extends BaseStore {
   };
 
   getSignInUrl = () => {
-    const {
-      REACT_APP_AUTH_URL: url,
-      REACT_APP_ID: clientId,
-      REACT_APP_CALLBACK_URL: callbackUrl
-    } = process.env;
+    const {REACT_APP_AUTH_URL: url, REACT_APP_ID: clientId} = process.env;
     const nonce = randomString.mixed(20);
     const state = randomString.mixed(20);
     stateStorage.set(state);
+
+    const callbackUrl = window.location.origin + '/auth';
 
     return `${url}/connect/authorize?client_id=${clientId}&scope=profile email address&response_type=token&redirect_uri=${encodeURIComponent(
       callbackUrl!
