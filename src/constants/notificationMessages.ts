038d138035f5d9a3b41f188a3019ab8c975134fe
g.moiseychik <g.moiseychik@itransition.com>
2018-08-27 19:07:27 +0300

    Revert "LWDEV-8793-rework-cancel-all-orders: reworked cancel all orders functionality (#346)"
    This reverts commit 4267dd8b88e3de99998830f6420e595fe207d764.

--- src/constants/notificationMessages.ts
+++ src/constants/notificationMessages.ts
@@ -3,9 +3,6 @@ const messages = {
   defaultError: 'Something went wrong',
   editOrderSuccess: 'Order was edited successfully',
   expired: 'Your session has expired',
-  allOrdersCancelled: 'All orders have been cancelled',
-  allCurrentInstrumentOrdersCancelled: (instrumentName: string) =>
-    `All ${instrumentName} orders have been cancelled`,
   orderCancelled: 'Order has been cancelled:',
   orderError: 'There is an error placing your order:',
   orderExecuted: (id: string) => `Order: ${id} was closed successfully`,
