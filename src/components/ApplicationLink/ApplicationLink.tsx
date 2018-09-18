28b10535c257e0f1579d3f5a652aa7fec1c9460d
Sergey Schegolev <31923498+sergeyschegolev@users.noreply.github.com>
2018-09-18 12:27:35 +0300

    Updates header component with new lykke react component (#372)
    Resolves LWDEV-9356

---
+++ src/components/ApplicationLink/ApplicationLink.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import {Link} from 'react-router-dom';
+
+interface ApplicationLinkProps {
+  classes: string;
+  title: JSX.Element;
+  url: string;
+}
+
+const ApplicationLink: React.SFC<ApplicationLinkProps> = ({
+  classes,
+  title,
+  url
+}) => {
+  if (!url.includes('http')) {
+    return (
+      <Link to={url} className={classes} key={url}>
+        {title}
+      </Link>
+    );
+  }
+
+  const customUrl = url === window.location.href ? undefined : url;
+  return (
+    <a href={customUrl} className={classes} key={url}>
+      {title}
+    </a>
+  );
+};
+
+export default ApplicationLink;
