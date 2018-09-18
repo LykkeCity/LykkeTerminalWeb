b2f0005865f2e636e1cf80e9cf8555d9bdf0a485
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-09-18 18:04:40 +0300

    Resolves orderbook records overlaps after updates and scroll bar appears at scale less than 100%

--- src/components/CustomScrollbar/CustomScrollbar.tsx
+++ src/components/CustomScrollbar/CustomScrollbar.tsx
@@ -5,18 +5,44 @@ import {dims} from '../styled';
 
 const defaultHeight = `calc(100% - ${rem(dims.tileHeaderHeight)}`;
 
-const CustomScrollbar: React.SFC<{autoHeight?: boolean; styles?: any}> = ({
-  children,
-  autoHeight,
-  styles
-}) => (
-  <Scrollbars
-    autoHeight={autoHeight}
-    autoHide={true}
-    style={{height: defaultHeight, ...styles}}
-  >
-    {children}
-  </Scrollbars>
-);
+const DEFAULT_MARGIN = 15;
+
+class CustomScrollbar extends React.Component<
+  {autoHeight?: boolean; styles?: any; onScrollStop?: any},
+  {}
+> {
+  private scrollRef: any;
+
+  componentDidMount() {
+    window.addEventListener('resize', (e: any) => {
+      const resizeCoefficient =
+        e.currentTarget.devicePixelRatio === 1
+          ? 1
+          : 1 / e.currentTarget.devicePixelRatio;
+      if (this.scrollRef) {
+        (this.scrollRef as any).view.style.marginRight = `-${DEFAULT_MARGIN *
+          resizeCoefficient}px`;
+        (this.scrollRef as any).view.style.marginBottom = `-${DEFAULT_MARGIN *
+          resizeCoefficient}px`;
+      }
+    });
+  }
+
+  setScrollbarRef = (scrollbar: any) => (this.scrollRef = scrollbar);
+
+  render() {
+    return (
+      <Scrollbars
+        autoHeight={this.props.autoHeight}
+        autoHide={true}
+        style={{height: defaultHeight, ...this.props.styles}}
+        ref={this.setScrollbarRef}
+        onScrollStop={this.props.onScrollStop}
+      >
+        {this.props.children}
+      </Scrollbars>
+    );
+  }
+}
 
 export default CustomScrollbar;
--- src/components/TradeList/index.tsx
+++ src/components/TradeList/index.tsx
@@ -1,10 +1,10 @@
 import {compose, pathOr} from 'rambda';
 import React from 'react';
-import Scrollbars from 'react-custom-scrollbars';
 import {withContentRect} from 'react-measure';
 import * as TradeFilterModelFns from '../../models/tradeFilter';
 import {withAuth} from '../Auth';
 import {connect} from '../connect';
+import {CustomScrollbar} from '../CustomScrollbar';
 import {withStyledScroll} from '../CustomScrollbar/withScroll';
 import {withKyc} from '../Kyc';
 import withLoader from '../Loader/withLoader';
@@ -90,9 +90,9 @@ const ConnectedTradeLog = connect(
     <React.Fragment>
       <ConnectedTradeLogHeader />
       <div style={{height: '100%'}} ref={measureRef}>
-        <Scrollbars
-          style={{
-            height: `calc(100% - 1.5rem)`,
+        <CustomScrollbar
+          styles={{
+            height: `calc(100% - 0.5rem)`,
             width: 'calc(100% + 1rem)',
             marginLeft: '-0.5rem'
           }}
@@ -101,7 +101,7 @@ const ConnectedTradeLog = connect(
             itemHeight={TRADE_HEIGHT}
             width={contentRect.client.width + LEFT_PADDING || 300}
           />
-        </Scrollbars>
+        </CustomScrollbar>
       </div>
     </React.Fragment>
   ))
