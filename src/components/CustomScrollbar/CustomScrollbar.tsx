d950763e950af6ec05cd735df5f1f6bef5769235
Sergey Schegolev <s.shchyogolev@itransition.com>
2018-09-19 11:26:26 +0300

    Adds additional margin for hide native scroll in some cases

--- src/components/CustomScrollbar/CustomScrollbar.tsx
+++ src/components/CustomScrollbar/CustomScrollbar.tsx
@@ -5,25 +5,47 @@ import {dims} from '../styled';
 
 const defaultHeight = `calc(100% - ${rem(dims.tileHeaderHeight)}`;
 
-const DEFAULT_MARGIN = 15;
+const DEFAULT_MARGIN = 17;
+const ADDITIONAL_MARGIN = 0.05;
 
 class CustomScrollbar extends React.Component<
   {autoHeight?: boolean; styles?: any; onScrollStop?: any},
   {}
 > {
   private scrollRef: any;
+  private startMarginRight: any;
+  private startMarginBottom: any;
 
   componentDidMount() {
+    if (this.scrollRef) {
+      (this.scrollRef as any).view.style.marginRight = `-${DEFAULT_MARGIN}px`;
+      (this.scrollRef as any).view.style.marginBottom = `-${DEFAULT_MARGIN}px`;
+    }
+
     window.addEventListener('resize', (e: any) => {
+      if (this.scrollRef && !this.startMarginBottom) {
+        this.startMarginBottom = (this
+          .scrollRef as any).view.style.marginBottom;
+      }
+      if (this.scrollRef && !this.startMarginRight) {
+        this.startMarginRight = (this.scrollRef as any).view.style.marginRight;
+      }
       const resizeCoefficient =
         e.currentTarget.devicePixelRatio === 1
           ? 1
-          : 1 / e.currentTarget.devicePixelRatio;
+          : 1 / e.currentTarget.devicePixelRatio + ADDITIONAL_MARGIN;
       if (this.scrollRef) {
-        (this.scrollRef as any).view.style.marginRight = `-${DEFAULT_MARGIN *
-          resizeCoefficient}px`;
-        (this.scrollRef as any).view.style.marginBottom = `-${DEFAULT_MARGIN *
-          resizeCoefficient}px`;
+        if (resizeCoefficient === 1) {
+          (this
+            .scrollRef as any).view.style.marginRight = this.startMarginRight;
+          (this
+            .scrollRef as any).view.style.marginBottom = this.startMarginBottom;
+        } else {
+          (this.scrollRef as any).view.style.marginRight = `-${DEFAULT_MARGIN *
+            resizeCoefficient}px`;
+          (this.scrollRef as any).view.style.marginBottom = `-${DEFAULT_MARGIN *
+            resizeCoefficient}px`;
+        }
       }
     });
   }
