4d3a00259b9c72727e1d0d4263f9ab6b05e5decc
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-11-12 15:12:09 +0500

    Temporarily disable Segment analytics (#405)
    Resolves LWDEV-10095

--- src/services/analyticsService.ts
+++ src/services/analyticsService.ts
@@ -1,16 +1,17 @@
 import {AnalyticsEventModel} from '../models/analyticsEventModel';
 
+// TODO: clarify the analytics suite
 export abstract class AnalyticsService {
   static handlePageLoading = () => {
-    window.analytics.load(process.env.REACT_APP_SEGMENT_WRITE_KEY);
-    window.analytics.page();
+    // window.analytics.load(process.env.REACT_APP_SEGMENT_WRITE_KEY);
+    // window.analytics.page();
   };
 
   static track = (event: AnalyticsEventModel) => {
-    window.analytics.track(event.title, event.details);
+    // window.analytics.track(event.title, event.details);
   };
 
   static handleIdentify = (traits: any): void => {
-    window.analytics.identify(traits);
+    // window.analytics.identify(traits);
   };
 }
