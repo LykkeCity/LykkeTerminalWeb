dbf1f6798c46c2eb77656adfa718f87d9decaf90
Andrey Zhylin <andrey.zhylin@outlook.com>
2019-01-09 13:44:44 +0300

    WEB-135: Added GTM

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,16 @@ import './Logger';
 import {RootStore} from './stores/index';
 import {getWorkerByCurrentBrowser} from './workers/worker';
 
+// tslint:disable-next-line:no-var-requires
+const TagManager = require('react-gtm-module');
+
+if (process.env.NODE_ENV === 'production') {
+  const tagManagerArgs = {
+    gtmId: 'GTM-5ZXBJWR'
+  };
+  TagManager.initialize(tagManagerArgs);
+}
+
 declare global {
   interface Window {
     analytics: any;
