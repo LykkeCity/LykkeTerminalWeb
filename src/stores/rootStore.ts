724f7f99dfea2b38a3f75d3c696ff5e118322c32
Ozgur GUL <lugrugzo@gmail.com>
2020-01-14 13:56:51 +0300

    WEB-228: Remove candle socket subscription

--- src/stores/rootStore.ts
+++ src/stores/rootStore.ts
@@ -131,10 +131,6 @@ class RootStore {
             topics.quoteAsk(x.id),
             this.referenceStore.onQuoteAsk
           );
-          this.socketStore.subscribe(
-            topics.candle('spot', x.id, this.uiStore.selectedPriceType, 'day'),
-            this.referenceStore.onCandle
-          );
         });
         this.uiStore.selectInstrument(
           this.uiStore.userSelectedInstrument ||
@@ -181,10 +177,6 @@ class RootStore {
         instruments.forEach(x => {
           subscribe(topics.quote(x.id), this.referenceStore.onQuote);
           subscribe(topics.quoteAsk(x.id), this.referenceStore.onQuoteAsk);
-          subscribe(
-            topics.candle('spot', x.id, this.uiStore.selectedPriceType, 'day'),
-            this.referenceStore.onCandle
-          );
         });
         this.uiStore.selectInstrument(
           this.uiStore.userSelectedInstrument ||
